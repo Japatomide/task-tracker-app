127e6ebe4b8868cdfd53d1f90461bef45245749c
Japatomide <jaiyeolapaul68@gmail.com>
2024-09-07 12:00:40 +0100

    first commit

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import Task from "./Task";
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
